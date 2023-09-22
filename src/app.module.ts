a08db3fb193d55d9fd73874deab770d98463eaea
Felipe Sanchez <f.sanxz@gmail.com>
2023-09-21 22:58:16 -0300

    add fipe client http implementation
    Co-authored-by: Gustavo Ifanger <ifanger@users.noreply.github.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReferenceService } from './reference/reference.service';
+import { ReferenceService } from './service/reference/reference.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ReferenceService],
 })
 export class AppModule {}
