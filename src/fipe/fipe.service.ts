7578f47d70eb73f1ebd32deb771760eaa62c585d
Felipe Sanchez <f.sanxz@gmail.com>
2023-09-28 03:18:23 -0300

    remove deprecated toPromise()

--- src/fipe/fipe.service.ts
+++ src/fipe/fipe.service.ts
@@ -1,7 +1,7 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable, Logger } from '@nestjs/common';
 import { AxiosResponse } from 'axios';
-import { Observable, map } from 'rxjs';
+import { Observable, firstValueFrom, map } from 'rxjs';
 import { Generic } from './models/generic';
 import { Reference } from './models/reference';
 import { BrandsRequest } from './dto/request/brands.request';
@@ -48,30 +48,34 @@ export class FipeService {
   }
 
   async runJob() {
-    const refs = await this.getReferences().toPromise();
-    const brands = await this.getBrands(
-      new BrandsRequest(refs![0].Codigo, 1),
-    ).toPromise();
+    const refs = await firstValueFrom(this.getReferences());
+    const brands = await firstValueFrom(
+      this.getBrands(new BrandsRequest(refs[0].Codigo, 1)),
+    );
 
     for (const brand of brands!) {
-      const models = await this.getModels(
-        new ModelsRequest(refs![0].Codigo, 1, +brand.Value),
-      ).toPromise();
-      for (const model of models!.Modelos) {
-        const years = await this.getYearsAndFuel(
-          new YearsRequest(refs![0].Codigo, 1, +brand.Value, +model.Value),
-        ).toPromise();
-        for (const year of years!) {
-          const vehicle = await this.getVehicle(
-            new VehicleRequest(
-              refs![0].Codigo,
-              1,
-              +brand.Value,
-              +model.Value,
-              +year.Value.split('-')[1],
-              +year.Value.split('-')[0],
+      const models = await firstValueFrom(
+        this.getModels(new ModelsRequest(refs[0].Codigo, 1, +brand.Value)),
+      );
+      for (const model of models.Modelos) {
+        const years = await firstValueFrom(
+          this.getYearsAndFuel(
+            new YearsRequest(refs[0].Codigo, 1, +brand.Value, +model.Value),
+          ),
+        );
+        for (const year of years) {
+          const vehicle = await firstValueFrom(
+            this.getVehicle(
+              new VehicleRequest(
+                refs[0].Codigo,
+                1,
+                +brand.Value,
+                +model.Value,
+                +year.Value.split('-')[1],
+                +year.Value.split('-')[0],
+              ),
             ),
-          ).toPromise();
+          );
           this.logger.debug('vehicle', JSON.stringify(vehicle));
         }
       }
